1f68f9079524fb8f7843f3ee098cbfe29a7d558d
monicaiyb <monicaiyb@gmail.com>
2024-04-02 22:33:44 +0300

    Add project files.

---
+++ TruckLoadinApp/Models/Account/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TruckLoadinApp.Models.Account
+{
+    public class ApplicationUser
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        [Key]
+        public int Id { get; set; }
+    }
+}
---
+++ TruckLoadinApp/Models/TruckLoadingContextDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TruckLoadinApp.Models.Account;
+
+namespace TruckLoadinApp.Models
+{
+    public class TruckLoadingContextDb : DbContext
+    {
+        public TruckLoadingContextDb(DbContextOptions<TruckLoadingContextDb> options)
+            : base(options)
+        {
+        }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+    }
+}
